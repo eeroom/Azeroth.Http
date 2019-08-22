c1d8c925bc109a6728dec2c67aa34815d21e533b
邓常青 <eedroom@163.com>
2019-08-22 17:19:53 +0800

    增加selfhost

---
+++ Azeroth.WebApiSelfHost/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Net.Http;
+namespace Azeroth.WebApiSelfHost.Controllers
+{
+    public class HomeController:ApiController
+    {
+        [HttpGet]
+        public List<int> GetEntities()
+        {
+            var lst = System.Linq.Enumerable.Range(0, 10).ToList();
+            return lst;
+        }
+    }
+}
---
+++ Azeroth.WebApiSelfHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Net.Http;
+namespace Azeroth.WebApiSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new System.Web.Http.SelfHost.HttpSelfHostConfiguration("http://localhost:8086");
+            config.Routes.MapHttpRoute("htt", "{controller}/{action}");
+
+            using (var server = new System.Web.Http.SelfHost.HttpSelfHostServer(config))
+            {
+                server.OpenAsync().Wait();
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
