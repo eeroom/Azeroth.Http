de325426da52cbda3ace655c1fd2dc50d330e32d
邓常青 <eedroom@163.com>
2019-08-22 17:27:31 +0800

    增加owin iis

---
+++ Azeroth.WebApiOwinIIS/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Net.Http;
+namespace Azeroth.WebApiOwinIIS.Controllers
+{
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public List<int> GetEntities()
+        {
+            var lst = System.Linq.Enumerable.Range(0, 10).ToList();
+            return lst;
+        }
+    }
+}
---
+++ Azeroth.WebApiOwinIIS/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Net.Http;
+using Owin;
+
+namespace Azeroth.WebApiOwinIIS
+{
+    public class Startup
+    {
+        public void Configuration(Owin.IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute("htt", "{controller}/{action}");
+            app.UseWebApi(config);
+        }
+    }
+}
