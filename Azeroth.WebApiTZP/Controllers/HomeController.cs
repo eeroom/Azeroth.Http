ef9d112be65e05435a07b4156f45ad69f9f2b5e8
邓常青 <eedroom@163.com>
2019-08-22 19:06:22 +0800

    增加aspnetcore aspnetcore mvc

---
+++ Azeroth.WebApiTZP/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+namespace Azeroth.WebApiTZP.Controllers
+{
+    public class HomeController:ControllerBase
+    {
+        [HttpGet]
+        public List<int> GetEntities()
+        {
+            var lst = System.Linq.Enumerable.Range(0, 10).ToList();
+            return lst;
+        }
+    }
+}
---
+++ Azeroth.WebApiTZP/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace Azeroth.WebApiTZP
+{
+    public class Startup : Microsoft.AspNetCore.Hosting.IStartup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc(HandlerMvcRoute);
+        }
+
+        
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(HandlerMvcOptions);
+            return services.BuildServiceProvider();
+        }
+
+        private void HandlerMvcOptions(MvcOptions options)
+        {
+                
+        }
+
+        private void HandlerMvcRoute(IRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapRoute("htt", "{controller}/{action}");
+        }
+    }
+}
