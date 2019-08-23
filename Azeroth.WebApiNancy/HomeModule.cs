850f463baeae0556057e30d55b2c71e9fa6f8e63
邓常青 <eedroom@163.com>
2019-08-23 19:14:42 +0800

    增加nancy

---
+++ Azeroth.WebApiNancy/HomeModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+namespace Azeroth.WebApiNancy
+{
+    public class HomeModule:NancyModule
+    {
+        public HomeModule()
+        {
+            this.Get["/"] = r => {
+                var os = System.Environment.OSVersion;
+                return "Hello Nancy<br/> System:" + os.VersionString;
+            };
+
+            this.Get["/aa"] = r => {
+                var os = System.Environment.OSVersion;
+                return "Hello rrrrrr<br/> System:" + os.VersionString;
+            };
+        }
+    }
+}
---
+++ Azeroth.WebApiNancy/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Owin;
+using Nancy.Owin;
+[assembly: Microsoft.Owin.OwinStartup(typeof(Azeroth.WebApiNancy.Startup))]
+namespace Azeroth.WebApiNancy
+{
+    public class Startup
+    {
+        public void Configuration(Owin.IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
