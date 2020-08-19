28a4d406b633c5959e042bc85847590bd7067087
邓常青 <eedroom@163.com>
2020-08-19 22:37:34 +0800

    像调用普通api方式调用asmx

--- Hg.Client/AsmxClientProxy.cs
+++ Hg.Client/AsmxClientProxy.cs
@@ -7,11 +7,46 @@ using System.Threading.Tasks;
 
 namespace Hg.Client
 {
-    class AsmxClientProxy : System.Runtime.Remoting.Proxies.RealProxy
+    public class AsmxClientProxy<T> : System.Runtime.Remoting.Proxies.RealProxy
     {
-        public override IMessage Invoke(IMessage msg)
+        public AsmxClient Client { get; private set; }
+        public AsmxClientProxy(string url):base(typeof(T))
         {
-            throw new NotImplementedException();
+             this.Client= new AsmxClient();
+            this.Client.Url = url;
+        }
+
+        public override IMessage Invoke(IMessage parameter)
+        {
+            var msg = parameter as System.Runtime.Remoting.Messaging.IMethodCallMessage;
+            var rt= this.Client.SendRequest(msg.MethodName, msg.Args);
+            var rtmsg = new System.Runtime.Remoting.Messaging.ReturnMessage(rt, null, 0, msg.LogicalCallContext, msg);
+            return rtmsg;
+        }
+
+        [System.Web.Services.WebServiceBinding(Namespace = "http://tempuri.org/")]
+        public class AsmxClient : System.Web.Services.Protocols.SoapHttpClientProtocol
+        {
+            static AsmxClient()
+            {
+                var assm = System.AppDomain.CurrentDomain.GetAssemblies()
+                    .Cast<System.Reflection.Assembly>()
+                    .FirstOrDefault(x => x.FullName.IndexOf("System.Web.Services,")==0);
+                if (assm == null)
+                    throw new Exception("未找到程序集System.Web.Services");
+                var sctMeta= assm.GetType("System.Web.Services.Protocols.SoapClientType", true);
+                var meta = typeof(T);
+                var sct= System.Activator.CreateInstance(sctMeta,
+                    System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic, 
+                    null, 
+                    new object[] { meta }, 
+                    System.Globalization.CultureInfo.CurrentCulture);
+                AddToCache(typeof(AsmxClientProxy<T>.AsmxClient), sct);
+            }
+            public object SendRequest(string methodName, object[] parameters)
+            {
+                return this.Invoke(methodName, parameters)[0];
+            }
         }
     }
 }
--- Hg.Client/Program.cs
+++ Hg.Client/Program.cs
@@ -10,6 +10,29 @@ namespace Hg.Client
     {
         static void Main(string[] args)
         {
+            string url = "http://localhost:50682/Home.asmx";
+            var home= (IHome)new AsmxClientProxy<IHome>(url).GetTransparentProxy();
+            var rt= home.HelloWorld(3);
+
+            var home2 = (IHome2)new AsmxClientProxy<IHome2>(url).GetTransparentProxy();
+            var rt2 = home2.HelloWorld(3);
+
+            var home3 = (IHome)new AsmxClientProxy<IHome>(url).GetTransparentProxy();
+            var rt3 = home3.HelloWorld(3);
         }
+    }
+
+    [System.Web.Services.WebServiceBinding(Namespace = "http://tempuri.org/")]
+     public interface IHome
+    {
+        [System.Web.Services.Protocols.SoapDocumentMethod]
+        string HelloWorld(int parameter);
+    }
+
+    [System.Web.Services.WebServiceBinding(Namespace = "http://tempuri.org/")]
+    public interface IHome2
+    {
+        [System.Web.Services.Protocols.SoapDocumentMethod]
+        string HelloWorld(int parameter);
     }
 }
