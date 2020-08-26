0dff8966d9b8d85d255eb55aa5c34238fbf10fc8
邓常青 <eedroom@163.com>
2020-08-26 23:22:11 +0800

    Azeroth.WcfTwoWayAuthentication

---
+++ Azeroth.WcfTwoWayAuthentication/Client.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Azeroth.WcfTwoWayAuthentication
+{
+    public partial class Client : System.Web.UI.Page
+    {
+        public string DateValue { get; set; }
+        protected override void OnLoad(EventArgs e)
+        {
+            using (var factory = new System.ServiceModel.ChannelFactory<IHome>("wch"))
+            {
+                var client = factory.CreateChannel();
+
+                this.DateValue = client.DoWork();
+            }
+        }
+    }
+}
