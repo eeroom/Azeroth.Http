4e160b6a921dd86d209416886e3abf55e15cbb7e
邓常青 <eedroom@163.com>
2021-03-31 22:53:25 +0800

    完善文件上传

--- Azeroth.Http.Util/Upload.ashx.cs
+++ Azeroth.Http.Util/Upload.ashx.cs
@@ -15,26 +15,24 @@ namespace Azeroth.Http.Util
         {
             if (context.Request.Files.Count < 1)
                 throw new ArgumentException("必须指定上传的文件内容");
-            long start = long.Parse(context.Request["start"]);
-            long size = long.Parse(context.Request["size"]);
-            var fileFolder= context.Server.MapPath("/uploadData");
+            long Position = long.Parse(context.Request["Position"]);
+            long FileSize = long.Parse(context.Request["FileSize"]);
+            var fileFolder= context.Server.MapPath("/UploadFiles");
             if (!System.IO.Directory.Exists(fileFolder))
                 System.IO.Directory.CreateDirectory(fileFolder);
-            var tempFileName = System.IO.Path.Combine(fileFolder, "_temp_" + context.Request.Files[0].FileName);
-            using (var filestream=new System.IO.FileStream(tempFileName, System.IO.FileMode.OpenOrCreate))
+            var filePath = System.IO.Path.Combine(fileFolder,   context.Request.Files[0].FileName);
+            using (var filestream=new System.IO.FileStream(filePath, System.IO.FileMode.OpenOrCreate))
             {
-                filestream.Position = start;
+                filestream.Position = Position;
                 context.Request.Files[0].InputStream.CopyTo(filestream);
                 filestream.Flush();
-                if (filestream.Length >= size)
-                {
-                    filestream.Close();
-                    System.IO.File.Move(tempFileName, System.IO.Path.Combine(fileFolder, context.Request.Files[0].FileName));
-                }
+                if (filestream.Length == FileSize)
+                    context.Response.Write("ok");
+                else if (filestream.Length < FileSize)
+                    context.Response.Write("ing");
+                else
+                    context.Response.Write("代码逻辑错误，服务端文件的大小超过浏览器端文件大小");
             }
-            
-            context.Response.Write("hello world");
-            context.Response.End();
         }
 
         public bool IsReusable
