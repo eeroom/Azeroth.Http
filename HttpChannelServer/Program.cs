f20a8b1cb4e98504ee58997605ee3c5f7d82f593
邓常青 <eedroom@163.com>
2021-05-26 22:57:14 +0800

    增加remoting的demo

---
+++ HttpChannelServer/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HttpChannelServer {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
