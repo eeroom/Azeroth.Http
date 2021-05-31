fa36a5d0b016df94ff4b5dfad6a6b41d41f2ba48
邓常青 <eedroom@163.com>
2021-06-01 00:11:47 +0800

    ------------

--- AsmxDemo/Home.asmx.cs
+++ AsmxDemo/Home.asmx.cs
@@ -23,11 +23,21 @@ namespace AsmxDemo
             return "Hello World"+a.ToString();
         }
 
+        /// <summary>
+        /// 配合axis类库的客户端自定义类型处理
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [WebMethod]
         public Student GetStudentById(int id) {
             return new Student() { Age = 101, Name = Guid.NewGuid().ToString() };
         }
 
+        /// <summary>
+        /// 配合axis类库客户端的数组处理，正常情况应该返回强类型的数据
+        /// 这里只是演示功能点
+        /// </summary>
+        /// <returns></returns>
         [WebMethod]
         public object[] Info() {
             var lst= System.Linq.Enumerable.Range(0, 10).Select(x=>(object)x).ToList();
