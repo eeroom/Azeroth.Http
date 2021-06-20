3facb3123b249bb6fa7c95ca860f701fe4daa461
邓常青 <eedroom@163.com>
2021-06-20 23:05:46 +0800

    增加SignalRSelfhost

---
+++ SignalRSelfhost/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRSelfhost {
+    class Program {
+        static void Main(string[] args) {
+            string baseAddress = "http://localhost:8016/";
+            // 启动 OWIN host
+            Microsoft.Owin.Hosting.WebApp.Start<Startup>(url: baseAddress);
+            Console.WriteLine("程序已启动,按任意键退出");
+            Console.ReadLine();
+        }
+    }
+}
