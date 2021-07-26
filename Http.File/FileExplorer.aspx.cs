eea2ca19e6da293ff2148ba22e2e24b83b319846
邓常青 <eedroom@163.com>
2021-07-27 00:40:08 +0800

    上传功能，分片，断点续传

--- Http.File/FileExplorer.aspx.cs
+++ Http.File/FileExplorer.aspx.cs
@@ -6,9 +6,37 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 
 namespace Http.File {
-    public partial class FileExplorer : System.Web.UI.Page {
-        protected void Page_Load(object sender, EventArgs e) {
+    public partial class FileExplorer : CmdPage<FileExplorer> {
+        
+        public object Upload(HttpContext context) {
+            if (context.Request.Files.Count < 1)
+                throw new ArgumentException("必须指定上传的文件内容");
+            long Position = long.Parse(context.Request["Position"]);
+            long FileSize = long.Parse(context.Request["FileSize"]);
+            string fullName = context.Request["WebkitRelativePath"];
+            fullName = String.IsNullOrWhiteSpace(fullName) ? context.Request.Files[0].FileName : fullName;
+            var rootFolder = context.Server.MapPath("");
+            rootFolder = System.IO.Directory.GetParent(rootFolder).Parent.FullName;
+            rootFolder = System.IO.Path.Combine(rootFolder, "Azeroth.File.UploadFiles");
 
+            var filePath = System.IO.Path.Combine(rootFolder, fullName);
+            var fileFolder = System.IO.Path.GetDirectoryName(filePath);
+            if (!System.IO.Directory.Exists(fileFolder))
+                System.IO.Directory.CreateDirectory(fileFolder);
+
+
+            using (var filestream = new System.IO.FileStream(filePath, System.IO.FileMode.OpenOrCreate)) {
+                filestream.Position = Position;
+                context.Request.Files[0].InputStream.CopyTo(filestream);
+                filestream.Flush();
+                if (filestream.Length == FileSize)
+                    context.Response.Write("ok");
+                else if (filestream.Length < FileSize)
+                    context.Response.Write("ing");
+                else
+                    context.Response.Write("代码逻辑错误，服务端文件的大小超过浏览器端文件大小");
+            }
+            return null;
         }
     }
 }
