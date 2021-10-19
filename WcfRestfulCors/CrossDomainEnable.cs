a6a72f3cec4a1caac3e9912717c0d853c9912dea
eeroom <eedroom@163.com>
2021-10-19 17:55:19 +0800

    完善cors的相关说明

--- WcfRestfulCors/CrossDomainEnable.cs
+++ WcfRestfulCors/CrossDomainEnable.cs
@@ -18,7 +18,13 @@ namespace WcfRestfulCors
         }
         public void OptionsHandler()
         {
-            //CORS协议的约定
+            //CORS协议的约定,关键响应头，
+            //Access-Control-Allow-Origin  必选，且后续每次响应都需要，这个值要么是请求头里面的Origin对应的值，要么为*，高版本浏览器对*不友好，有额外限制
+            //Access-Control-Allow-Methods 必选
+            //Access-Control-Max-Age   必须，单位为秒
+            //Access-Control-Allow-Headers 可选，如果请求头有Access-Control-Request-Headers，则必须，可以多个，逗号分隔，不限于预检请求头中Access-Control-Request-Headers的值
+            //Access-Control-Allow-Credentials 可选，影响是否发送cookie和http认证信息和浏览器是否允许js读取响应中的cookie,要么值为true,要么没有这个响应头，不存在false；配合这个功能，浏览器的xhr对象的.withCredentials属性设置为true
+            //Access-Control-Expose-Headers 可选，浏览器默认只能拿到6个响应头的值，如果需要额外的，就把额外的响应头名称在这里知道
             var context = System.ServiceModel.Web.WebOperationContext.Current;
             //context.OutgoingResponse.Headers.Add("Access-Control-Allow-Origin", "*");
             context.OutgoingResponse.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT");
