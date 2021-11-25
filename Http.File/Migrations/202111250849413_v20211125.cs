88b69003c4ecbb8c395d81ddb5b68adbc9d3bcd2
eeroom <eedroom@163.com>
2021-11-25 16:58:47 +0800

    更新数据库

---
+++ Http.File/Migrations/202111250849413_v20211125.cs
@@ -0,0 +1,26 @@
+namespace Http.File.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class v20211125 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.FileEntity", "FullName", c => c.String(nullable: false, maxLength: 255));
+            AddColumn("dbo.FileEntity", "ClientHashValue", c => c.String(nullable: false, maxLength: 4000));
+            AlterColumn("dbo.FileEntity", "Size", c => c.Long(nullable: false));
+            DropColumn("dbo.FileEntity", "Name");
+            DropColumn("dbo.FileEntity", "Folder");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.FileEntity", "Folder", c => c.String(nullable: false, maxLength: 255));
+            AddColumn("dbo.FileEntity", "Name", c => c.String(nullable: false, maxLength: 255));
+            AlterColumn("dbo.FileEntity", "Size", c => c.Int(nullable: false));
+            DropColumn("dbo.FileEntity", "ClientHashValue");
+            DropColumn("dbo.FileEntity", "FullName");
+        }
+    }
+}
