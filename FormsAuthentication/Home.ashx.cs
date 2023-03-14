1e326f57c8890169af5b5cda3afc30d7412dd9e1
eeroom <eedroom@163.com>
2023-03-14 19:45:59 +0800

    表单校验

---
+++ FormsAuthentication/Home.ashx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormsAuthentication
+{
+    /// <summary>
+    /// Home 的摘要说明
+    /// </summary>
+    public class Home : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Home当前用户：" + context.User.Identity.Name);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ FormsAuthentication/Login.ashx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormsAuthentication
+{
+    /// <summary>
+    /// Login 的摘要说明
+    /// </summary>
+    public class Login : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            var userName= context.Request["userName"];
+            if (string.IsNullOrEmpty(userName))
+            {
+                context.Response.Write("请输入用户名");
+                return;
+            }
+            var pwd = context.Request["pwd"];
+            if (string.IsNullOrEmpty(pwd))
+            {
+                context.Response.Write("请输入密码");
+                return;
+            }
+            if (pwd != "123456")
+            {
+                context.Response.Write("用户名或密码错误");
+                return;
+            }
+            System.Web.Security.FormsAuthentication.RedirectFromLoginPage(userName, true);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
