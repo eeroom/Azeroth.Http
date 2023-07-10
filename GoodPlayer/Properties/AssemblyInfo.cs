bc9c2f47b94627e1fc50d668f761e69d4a4e8bbb
eeroom <eedroom@163.com>
2023-07-10 21:15:26 +0800

    优化命名

--- GoodPlayer/Properties/AssemblyInfo.cs
+++ GoodPlayer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("HttpFileClient")]
+[assembly: AssemblyTitle("GoodPlayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HttpFileClient")]
+[assembly: AssemblyProduct("GoodPlayer")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
