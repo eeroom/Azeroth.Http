a2e2b5a52089785e30a4f2fc41a78f08b7c6addc
eeroom <eedroom@163.com>
2023-07-11 15:48:21 +0800

    WCF4TwoWayAuthen

--- TwoWayAuthenticationWcf/Home.svc.cs
+++ TwoWayAuthenticationWcf/Home.svc.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
 
-namespace TwoWayAuthenticationWcf
+namespace WCF4TwoWayAuthen
 {
     // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“Home”。
     // 注意: 为了启动 WCF 测试客户端以测试此服务，请在解决方案资源管理器中选择 Home.svc 或 Home.svc.cs，然后开始调试。
